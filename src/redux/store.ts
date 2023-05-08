9f1ad5c06ba2cb52bc320371cf99cb7240bf1030
piotrmigas <piotr.migas@icloud.com>
2023-05-08 14:30:09 +0200

    export fix

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,7 +2,6 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import thunk from 'redux-thunk';
 import storage from 'redux-persist/lib/storage';
-export type RootState = ReturnType<typeof rootReducer>;
 import taskReducer from './taskSlice';
 import listReducer from './listSlice';
 
@@ -19,6 +18,8 @@ export const store = configureStore({
   reducer: persistedReducer,
   middleware: [thunk],
 });
+
+export type RootState = ReturnType<typeof rootReducer>;
 
 export type AppDispatch = typeof store.dispatch;
 
